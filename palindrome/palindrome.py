4c258d7753438bd846b06d790b82c30ddc9b8f43
Salvador Cuamatzi <escuamatzi@gmail.com>
2013-04-17 18:59:35 -0500

    First attempt for the palindrome script

---
+++ palindrome/palindrome.py
@@ -0,0 +1,13 @@
+def is_palindrome(s):
+	"""
+	(str) -> Bool
+	Return True if and only if s is palindrome.
+
+	>>>is_palindrome('noon')
+	True
+	>>>is_palindrome('rececar')
+	True
+	>>>is_palindrome('dented')
+	False
+	"""
+
