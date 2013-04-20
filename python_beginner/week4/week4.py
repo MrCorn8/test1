442e29beeab6da7cef7ae38ce255f181093d6962
Salvador Cuamatzi <escuamatzi@gmail.com>
2013-04-19 19:06:20 -0500

    files for the week 4 of python beginners at coursera

---
+++ python_beginner/week4/week4.py
@@ -0,0 +1,19 @@
+print "we" in "week"
+
+print len("abracadabra")
+
+
+#String methods
+s="I'm late! I'm late! For a very important date!"
+print s.lower()
+
+# dir(s) give us the methos for the string or dir(str)
+# help(str.lower) gives us the explanation of the method
+
+print s.count("ate")
+
+print s.find("late")
+print s.find("late",7)
+
+s="       Many spaces before and after this.     "
+print s.strip()
