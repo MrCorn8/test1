14fd6640f80eea7be93f82c195cd3b09fef07d75
Salvador Cuamatzi <escuamatzi@gmail.com>
2013-05-23 16:34:24 -0500

    program works, useless code deleted, instructions included

--- video-split/stripmovie.py
+++ video-split/stripmovie.py
@@ -1,6 +1,17 @@
 import timedifference
 import os
 
+#INSTRUCTIONS
+#This programs reads the file "movie.txt" with the following structure:
+
+#filename_of_the_movie_including_extension
+#video bitrate (only numbers)
+#audio bitrate (only numbers)
+#range of time separated by "," like: 1:25,2:35, one per line
+
+#The file timedifference.py has to be in the same directory
+
+
 #Read file
 file=open("movie.txt",'r')
 data=file.readlines()
@@ -14,24 +25,15 @@ newfilename=changefilename[0]+".mp4"
 vbitrate=data[1].strip()
 abitrate=data[2].strip()
 
-#print(vbitrate)
-#print(abitrate)
 #last lines are the list of times for the stripping, make the list of commands
 commandlist=[]
 for i in range(3,len(data)):
 	times=data[i].strip().split(",")
-#	print("times")
-#	print(times)
 	offset=timedifference.toseconds(times[0])
 	duration=timedifference.timediff(times[1],times[0])
 	outputname=newfilename[:-4]+str(i-2)+newfilename[-4:]
 
-#	print("offset= "+str(offset))
-#	print("duration= "+str(duration))
-	#print("avconv -ss "+str(offset)+" -i \""+str(filename)+"\" -t "+str(duration)+" -acodec libmp3lame -b:a "+str(abitrate)+"k -b:v "+str(vbitrate)+"k \""+outputname+"\"")
-	commandlist.append("avconv -ss "+str(offset)+" -i \""+str(filename)+"\" -t "+str(duration)+" -acodec libmp3lame -b:a "+str(abitrate)+"k -b:v "+str(vbitrate)+"k \""+outputname+"\"")
-
-print(commandlist)
+	commandlist.append("avconv -i \""+str(filename)+"\" -t "+str(duration)+" -ss "+str(offset)+" -acodec libmp3lame -b:a "+str(abitrate)+"k -b:v "+str(vbitrate)+"k \""+outputname+"\"")
 
 #iterate over the list of commands
 for command in commandlist:
